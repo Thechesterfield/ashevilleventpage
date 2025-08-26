0c572175becb40abfb11d0c043505b8ae43f3b96
Chad <chad@optimal.agency>
2025-08-26 13:59:16 -0400

    Revert "Revert "Add database connection and sample events functionality""
    This reverts commit 7365065b09b771ab50de0b29d35b898bfcf76f08.

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -62,11 +63,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, async () => {
+  server.listen(port, "localhost", async () => {
     log(`serving on port ${port}`);
     
     // Initialize database with venues
